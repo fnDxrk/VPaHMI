0d919cc457b6c2c35741a0603729996039d3daf7
Phlipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-02-02 15:28:47 +0700

    First program

--- Домашнее задание №1. Диаграммы классов/ConsoleApp/Program.cs
+++ Домашнее задание №1. Диаграммы классов/ConsoleApp/Program.cs
@@ -1,2 +1,3 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите свое имя: ");
+var name = Console.ReadLine();
+Console.WriteLine($"Привет, {name}");
