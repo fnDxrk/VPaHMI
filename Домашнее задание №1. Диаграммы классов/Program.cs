bb573458f8cf278ba6b499f21eb247df8ef2106a
Phlipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-02-02 19:41:27 +0700

    Test project

--- Домашнее задание №1. Диаграммы классов/Program.cs
+++ Домашнее задание №1. Диаграммы классов/Program.cs
@@ -1,1 +1,17 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Homework_1.Account;
+using Homework_1.EMail;
+using Homework_1.SMS;
+
+// Accounts
+Account person1 = new Account(1400);
+
+// SMS
+SMSLowBalanceNotifyer sms1 = new SMSLowBalanceNotifyer("+77777777777", 300);
+SMSLowBalanceNotifyer sms2 = new SMSLowBalanceNotifyer("+7800553535", 700);
+SMSLowBalanceNotifyer sms3 = new SMSLowBalanceNotifyer("+73136663242", 1000);
+
+// EMail
+EMailBalanceChangedNotifyer email1 = new EMailBalanceChangedNotifyer("pochta.noname@gmail.com");
+
+person1.AddNotify(sms1);
+person1.ChangeBalance(299);
