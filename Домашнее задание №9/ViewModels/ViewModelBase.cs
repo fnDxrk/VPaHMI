6e754caeee9cfc4a34125d907e21beb27870ccb5
Phlipp Andruschenko <dxrk.rzn7txt@gmail.com>
2024-03-31 17:06:02 +0700

    Create project 9

---
+++ Домашнее задание №9/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using ReactiveUI;
+
+namespace Домашнее_задание__9.ViewModels;
+
+public class ViewModelBase : ReactiveObject
+{
+}
---
+++ Домашнее задание №9/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Домашнее_задание__9.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
